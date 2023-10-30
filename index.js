b9476023590047ebc8bcf9c3f6589f9982dc6f43
Billy0923 <Billy5279911@gmail.com>
2023-10-30 16:42:11 -0400

    implemented three request

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import './public/index.html'
 // import the pets array from data.js
 const pets = require('./data');
 
@@ -6,11 +7,12 @@ const express = require('express');
 const app = express();
 
 const PORT = 8080;
+app.use(express.static('public'))
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    
 });
 
 // hello world route
@@ -21,31 +23,31 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets)
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
+app.get('/api/v1/pets/:owner', (req, res) => {
     // get the owner from the request
-
+    const owner= req.params.owner
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(pet)
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(pet)
 });
 
 app.listen(PORT, () => {
